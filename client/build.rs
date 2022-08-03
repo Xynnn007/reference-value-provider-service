eecb2a579a166a05822e52b4d93ecae427220324
Xynnn007 <mading.ma@alibaba-inc.com>
2022-08-03 02:48:06 +0000

    feat: add client
    Signed-off-by: Xynnn007 <mading.ma@alibaba-inc.com>

--- client/build.rs
+++ client/build.rs
@@ -1,6 +1,5 @@
 fn main() -> shadow_rs::SdResult<()> {
     tonic_build::compile_protos("../protos/control.proto")?;
-    tonic_build::compile_protos("../protos/query.proto")?;
 
     shadow_rs::new()
 }
